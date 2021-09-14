4829937c52159b1a7d4640dff449afae95371657
Dell <Dell@ZISHAN>
2021-09-14 18:39:57 +0530

    Add project files.

---
+++ refKeyword/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace refKeyword
+{
+    class swap
+    {
+        public void Swapping(ref int a,ref int b)
+        {
+            int temp;
+            temp = a;
+            a = b;
+            b = temp;
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            swap obj = new swap();
+            Console.WriteLine("Enter Two Numbers");
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+            obj.Swapping(ref x, ref y);
+            Console.WriteLine("x:{0}, y:{1}", x, y);
+               
+        }
+    }
+}
